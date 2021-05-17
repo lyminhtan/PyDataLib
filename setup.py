65e473b7c294021ceed9ee57700d8cfc0fede552
lyminhtan <lyminhtan@gmail.com>
2021-05-17 10:50:16 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='pydatalib',
+    version='0.0.1',
+    packages=['mailnotify'],
+    url='',
+    license='MIT',
+    author='Tan Minh Ly',
+    author_email='lyminhtan@gmail.com',
+    description='This is a package to solve some Data Analyst\'s needs',
+    #install_requires = [
+       #'validators',
+       #'beautifulsoup4',
+    #],
+)
